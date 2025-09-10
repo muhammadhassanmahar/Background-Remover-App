627f44237259cd87827dd0feb38f07c2ea31249f
Muhammad Hassan <immaharhasaan@gmail.com>
2025-09-10 10:05:34 +0500

    your message

---
+++ lib/utills/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.blueAccent;
+  static const Color secondary = Colors.greenAccent;
+  static const Color background = Color(0xFF1E2A38);
+  static const Color textLight = Colors.white70;
+  static const Color textDark = Colors.black87;
+}
