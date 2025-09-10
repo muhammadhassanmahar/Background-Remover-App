5876daf273961ae3397d5e6f715f20f54a687f45
Muhammad Hassan <immaharhasaan@gmail.com>
2025-09-10 10:13:27 +0500

    your message

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,26 +1,34 @@
+import 'dart:io';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import '../utils/app_constants.dart';
 
 class ApiService {
-  static Future<Uint8List?> removeBackground(
-      String filePath) async {
+  /// Remove background from image
+  static Future<Uint8List?> removeBackground(File imageFile) async {
     try {
-      var request =
-          http.MultipartRequest('POST', Uri.parse(AppConstants.apiUrl));
-      request.headers.addAll({
-        'x-rapidapi-key': AppConstants.apiKey,
-        'x-rapidapi-host': AppConstants.apiHost,
-      });
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse("${AppConstants.baseUrl}/remove-background"),
+      );
 
-      request.files.add(await http.MultipartFile.fromPath('image', filePath));
+      // Attach image file
+      request.files.add(
+        await http.MultipartFile.fromPath('image_file', imageFile.path),
+      );
+
+      // Headers
+      request.headers.addAll({
+        "X-RapidAPI-Key": AppConstants.apiKey,
+        "X-RapidAPI-Host": AppConstants.apiHost,
+      });
 
       var response = await request.send();
 
       if (response.statusCode == 200) {
         return await response.stream.toBytes();
       } else {
-        throw Exception("Failed: ${response.reasonPhrase}");
+        throw Exception("Failed with status ${response.statusCode}");
       }
     } catch (e) {
       rethrow;
