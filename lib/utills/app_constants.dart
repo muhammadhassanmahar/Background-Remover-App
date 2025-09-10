568bf904b8d5ede493d630ef0538148b7a7a389a
Muhammad Hassan <immaharhasaan@gmail.com>
2025-09-10 10:11:15 +0500

    your message

--- lib/utills/app_constants.dart
+++ lib/utills/app_constants.dart
@@ -1,6 +1,5 @@
 class AppConstants {
-  // RapidAPI details (replace with your API key)
-  static const String apiKey = "YOUR_RAPIDAPI_KEY_HERE";
+  static const String apiKey = "YOUR_RAPIDAPI_KEY_HERE"; // apni key dalna
   static const String apiHost = "background-removal-api.p.rapidapi.com";
   static const String apiUrl =
       "https://background-removal-api.p.rapidapi.com/remove";
