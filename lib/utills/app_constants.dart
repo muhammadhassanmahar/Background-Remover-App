fe36e18930dc671950e1a74677f0538162908e9d
Muhammad Hassan <immaharhasaan@gmail.com>
2025-09-10 10:06:22 +0500

    your message

--- lib/utills/app_constants.dart
+++ lib/utills/app_constants.dart
@@ -1,8 +1,7 @@
 class AppConstants {
-  static const String baseUrl =
-      "https://background-remover.p.rapidapi.com";
-  static const String apiKey =
-      "ebe6a5ab82msh1bd763ce2b1eaa9p1411b4jsnaf56b22a4e4e"; // ⚠️ replace in production
-  static const String apiHost =
-      "background-remover.p.rapidapi.com";
+  // RapidAPI details (replace with your API key)
+  static const String apiKey = "YOUR_RAPIDAPI_KEY_HERE";
+  static const String apiHost = "background-removal-api.p.rapidapi.com";
+  static const String apiUrl =
+      "https://background-removal-api.p.rapidapi.com/remove";
 }
