96faf3ef1cf59f38bd5c8f60cae5ac27659f1445
Muhammad Hassan <immaharhasaan@gmail.com>
2025-09-11 19:29:59 +0500

    your commit message

--- lib/utills/app_constants.dart
+++ lib/utills/app_constants.dart
@@ -1,11 +1,11 @@
 class AppConstants {
-  // Tumhari RapidAPI key
+  // 🔑 Tumhari RapidAPI key
   static const String apiKey =
       "ebe6a5ab82msh1bd763ce2b1eaa9p1411b4jsnaf56b22a4e4e";
 
-  // RapidAPI Host (✅ Corrected)
+  // 🌍 RapidAPI Host (✅ Corrected)
   static const String apiHost = "background-remover.p.rapidapi.com";
 
-  // Base API URL (✅ Corrected)
-  static const String apiUrl = "https://background-remover.p.rapidapi.com";
+  // 📡 Base API URL (✅ Corrected)
+  static const String apiUrl = "https://background-remover.p.rapidapi.com/remove";
 }
